41237adbb439810a06020ebdb756bc79e21e1a2e
Florent Benoit <Florent.Benoit@serli.com>
2015-02-09 10:03:10 +0100

    UD-379 Add sourcemaps

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -19,6 +19,7 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('scripts', function () {
   return gulp.src(paths.src + '/{app,components}/**/*.js')
+    .pipe($.sourcemaps.init())
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($['6to5']())
@@ -27,13 +28,14 @@ gulp.task('scripts', function () {
       this.emit('end');
     })
     .pipe($.ngAnnotate())
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest(paths.tmp + '/6to5'))
     .pipe($.size());
 });
 
 gulp.task('browserify', ['scripts'], function () {
   return gulp.src(paths.tmp + '/6to5/app/index.js', { read: false })
-    .pipe($.browserify())
+    .pipe($.browserify( {debug : true}))
     .on('error', function handleError(err) {
       console.error(err.toString());
       this.emit('end');
