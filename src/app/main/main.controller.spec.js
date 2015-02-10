f22f918e6545544f27e90dfe9271804412724df0
Florent Benoit <Florent.Benoit@serli.com>
2015-02-10 11:36:48 +0100

    License headers

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2015 Codenvy, S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,8 +7,7 @@
  *
  * Contributors:
  *   Codenvy, S.A. - initial API and implementation
- *******************************************************************************/
-
+ */
 'use strict';
 
 describe('controllers', function(){
--- src/app/projects/projects.controller.spec.js
+++ src/app/projects/projects.controller.spec.js
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2015 Codenvy, S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,4 +7,4 @@
  *
  * Contributors:
  *   Codenvy, S.A. - initial API and implementation
- *******************************************************************************/
+ */
