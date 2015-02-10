da44851c29dadbbf508ed0059bf3fd44c26822e2
Florent Benoit <Florent.Benoit@serli.com>
2015-02-10 11:55:22 +0100

    UD-403 Add other fonts file

--- gulp/build.js
+++ gulp/build.js
@@ -80,7 +80,7 @@ gulp.task('images', function () {
 
 gulp.task('fonts', function () {
   return gulp.src($.mainBowerFiles())
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.filter('**/*.{eot,svg,ttf,otf,woff,woff2}'))
     .pipe($.flatten())
     .pipe(gulp.dest(paths.dist + '/fonts/'));
 });
