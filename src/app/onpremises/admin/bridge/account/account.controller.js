10a7d6614a366f198d2364e4eb1fd0218c23aaf9
Mickaël Leduque <mickael.leduque@serli.com>
2015-04-28 17:39:41 +0200

    CDEC-159 Start account section of bridge page

---
+++ src/app/onpremises/admin/bridge/account/account.controller.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
+'use strict';
+
+class OnPremisesAdminBridgeCodenvyAccountCtrl {
+
+  /**
+   * Default constructor.
+   * @ngInject for Dependency injection
+   */
+  constructor() {
+    this.onpremSubscriptionExpiration = new Date();
+  }
+
+  hasOnPremSubscription() {
+    return true;
+  }
+  
+  loggedOnSaaS() {
+    return false;
+  }
+
+  loginDisabled() {
+    return this.userName == null;
+  }
+}
+
+export default OnPremisesAdminBridgeCodenvyAccountCtrl;
