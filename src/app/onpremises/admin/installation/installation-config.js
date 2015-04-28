ca9489fcc09f57a2207bfe478c7356ede2105676
Mickaël Leduque <mickael.leduque@serli.com>
2015-04-28 17:41:01 +0200

    CDEC-157 Placeholder for installation configuration page

---
+++ src/app/onpremises/admin/installation/installation-config.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
+'use strict';
+
+import OnPremisesAdminInstallationCtrl from "./installation.controller";
+
+class OnPremisesAdminInstallationConfig {
+
+  constructor(register) {
+
+    register.controller('OnPremisesAdminInstallationCtrl', OnPremisesAdminInstallationCtrl);
+
+    // configure routes
+    register.app.config(function ($routeProvider) {
+      $routeProvider.when('/onpremises/admin/installation', {
+        templateUrl: 'app/onpremises/admin/installation/installation.html',
+        controller: 'OnPremisesAdminInstallationCtrl',
+        controllerAs: 'onPremisesAdminInstallationCtrl'
+      });
+    });
+  }
+}
+
+export default OnPremisesAdminInstallationConfig;
