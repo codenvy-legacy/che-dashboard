dbcdc3e7ee480ddd90939cbf0cb0fb51fe37e426
Florent Benoit <fbenoit@codenvy.com>
2015-04-28 14:10:47 +0200

    UD-504 Welcome page step #1 Introduce icons for on boarding and controller

---
+++ src/app/onboard/onboard-config.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
+'use strict';
+
+import OnBoardCtrl from './onboard.controller';
+
+class OnBoardConfig {
+
+  constructor(register) {
+    register.controller('OnBoardCtrl', OnBoardCtrl);
+
+    // config routes
+    register.app.config(function ($routeProvider) {
+      $routeProvider.when('/welcome', {
+        templateUrl: 'app/onboard/onboard.html',
+        controller: 'OnBoardCtrl',
+        controllerAs: 'onBoardCtrl'
+      });
+    })
+    ;
+  }
+}
+
+
+export default OnBoardConfig;
