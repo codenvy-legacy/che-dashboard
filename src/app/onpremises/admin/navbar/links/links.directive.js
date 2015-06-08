911158515affb3f0537c3ddca2774d015f62a010
Mickaël Leduque <mickael.leduque@serli.com>
2015-06-08 17:01:47 +0200

    Create a directive for links section in admin navbar

---
+++ src/app/onpremises/admin/navbar/links/links.directive.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
+'use strict';
+
+/**
+ * Defines a directive for creating the links side navbar panel.
+ */
+class AdminLinksPanel {
+
+  /**
+   * Default constructor that is using resource
+   * @ngInject for Dependency injection
+   */
+  constructor () {
+    this.restrict = 'E';
+    this.replace = false;
+    this.templateUrl = 'app/onpremises/admin/navbar/links/links.html';
+    this.controller = 'AdminLinksCtrl';
+    this.controllerAs = 'adminLinksCtrl';
+  }
+
+}
+
+export default AdminLinksPanel;
