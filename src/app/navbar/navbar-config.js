738c3e84caf3e4a3df3809cc493cfb6269f728e9
Florent Benoit <fbenoit@codenvy.com>
2015-06-25 09:45:39 +0200

    UD-593 Implement state for the left menu bar and not only use focus

--- src/app/navbar/navbar-config.js
+++ src/app/navbar/navbar-config.js
@@ -12,6 +12,7 @@
 
 import NavBarCtrl from '../navbar/navbar.controller';
 import NavBar from '../navbar/navbar.directive.js';
+import NavBarSelected from '../navbar/navbar-selected.directive.js';
 import UdBetaWidget from '../navbar/ud-beta-widget.directive.js';
 
 class NavbarConfig {
@@ -19,7 +20,7 @@ class NavbarConfig {
   constructor(register) {
     register.controller('NavbarCtrl', NavBarCtrl);
     register.directive('cdvyNavBar', NavBar);
-
+    register.directive('navBarSelected', NavBarSelected);
     register.directive('udBetaWidget', UdBetaWidget);
   }
 }
---
+++ src/app/navbar/navbar-selected.directive.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name navbar.selected.directive:NavBarSelected
+ * @description This class is adding a CSS class when element is clicked
+ * @author Florent Benoit
+ */
+class NavBarSelected {
+
+  /**
+   * Default constructor that is using resource
+   * @ngInject for Dependency injection
+   */
+  constructor ($rootScope) {
+    this.$rootScope = $rootScope;
+    this.restrict = 'A';
+    this.replace = false;
+  }
+
+
+  /**
+   * Monitor click
+   */
+  link($scope, element, attrs, controller) {
+    element.bind('click', () => {
+
+      // if there is a previous selected element, unselect it
+      if (this.$rootScope.selectedNavBarElement) {
+        this.$rootScope.selectedNavBarElement.removeClass('cdvy-navbar-selected');
+      }
+      // select the new element
+      this.$rootScope.selectedNavBarElement = element;
+      // add the class
+      element.addClass('cdvy-navbar-selected');
+
+    });
+
+  }
+
+
+}
+
+export default NavBarSelected;
