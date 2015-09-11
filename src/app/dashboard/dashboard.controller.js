5c575e0e35a3a482f7e82e22bc22778a6b5ee3a3
Anatoliy Bazko <abazko@codenvy.com>
2015-09-11 09:46:13 +0300

    Generate analytics event for 1st time user hits welcome screen

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -23,11 +23,31 @@ class DashboardCtrl {
    * Default constructor
    * @ngInject for Dependency injection
    */
-  constructor(codenvyAPI) {
+  constructor(codenvyAPI, codenvyUser, imsPropertiesApi, imsEventLoggingApi) {
     this.isSaasServiceAvailable = codenvyAPI.getService().isServiceAvailable(codenvyAPI.getSaas().getSaasServicePath());
+    this.imsPropertiesApi = imsPropertiesApi;
+    this.imsEventLoggingApi = imsEventLoggingApi;
+    this.codenvyUser = codenvyUser;
+
+    this.logEventOnAdminFirstLogin();
   }
 
-
+  /**
+   * Log specific event if it is first admin login.
+   */
+  logEventOnAdminFirstLogin() {
+    if (this.codenvyUser.isAdmin()) {
+      let property = this.imsPropertiesApi.getProperty('firstAdminLoginDone');
+      if (!property || property !== 'true') {
+        let promise = this.imsEventLoggingApi.logSaasCdecFirstLoginEvent();
+        promise.then(() => {
+          this.imsPropertiesApi.storeProperty('firstAdminLoginDone', 'true');
+        }, (error) => {
+            console.log('error', error);
+        });
+      }
+    }
+  }
 }
 
 export default DashboardCtrl;
