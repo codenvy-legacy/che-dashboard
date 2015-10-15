1c7890a36b9fe903977b510bf4788b1051199d48
Florent BENOIT <fbenoit@codenvy.com>
2015-10-15 16:59:11 +0200

    revert commit. PR is required
    Change-Id: Iea08c77a1a10b1f674e8a172dfa3bdcd186f14d7
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Set docker image:
-# docker pull vkuznyetsov/odyssey
-# 
-# To use docker without sudo:
-# sudo groupadd docker
-# sudo gpasswd -a ${USER} docker
-# log out and log back in again.
-docker run -i -t -e USRID=$(id -u) -e USRGR=$(id -g) -v $(pwd):/home/user/app -v ${HOME}/.m2:/home/user/.m2 vkuznyetsov/user-dashboard-java8 sh -c 'sudo chown user:user -R /home/user/app && sudo chown user:user -R /home/user/.m2 && mvn clean install; sudo chown $USRID:$USRGR -R /home/user/app; sudo chown $USRID:$USRGR -R /home/user/.m2'
